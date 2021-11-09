a991a4c6fcf95ccd030753fe5520bd9b47697c1d
JuliLarionova <julilarionova@yandex.ru>
2021-11-09 20:28:41 +0100

    little refactoring

--- src/test/java/com/cpgmn/account/service/TestDataFactory.java
+++ src/test/java/com/cpgmn/account/service/TestDataFactory.java
@@ -8,9 +8,7 @@ import com.cpgmn.account.web.dto.CreatedAccountDto;
 import com.cpgmn.account.web.dto.CustomerDto;
 
 import java.math.BigDecimal;
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.Collections;
 import java.util.UUID;
 
@@ -19,8 +17,7 @@ import static java.util.UUID.fromString;
 public class TestDataFactory {
     public static final UUID CUSTOMER_ID = fromString("a7e7ba88-35b9-11ec-bd22-bb29106ca7af");
     public static final UUID ACCOUNT_ID = fromString("a7e85682-35b9-11ec-bd23-df2635677886");
-    public static final Instant CREATION_DATE_INSTANT = Instant.parse("2021-11-06T10:15:30.00Z");
-    public static final LocalDateTime ACCOUNT_CREATION_DATE = LocalDateTime.ofInstant(CREATION_DATE_INSTANT, ZoneOffset.UTC);
+    public static final LocalDateTime ACCOUNT_CREATION_DATE = LocalDateTime.parse("2021-11-06T10:15:30.00");
     public static final String CUSTOMER_NAME = "Mikhail";
     public static final String CUSTOMER_SURNAME = "Larionov";
 
